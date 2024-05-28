975a78a997a62e19d4ab555ba3b2ba5ecac517f4
singh043 <kumarjatin431109@gmail.com>
2024-05-28 21:04:05 +0530

    Revert "bug fixes"
    This reverts commit 3f4971a6ea2e014bed95f49d8d27e7b85889659a.

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,12 +5,12 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  apiKey: "AIzaSyDYZKdblLeP1DYXv3JdhKWVuU91jmWXtQM",
+  authDomain: "chat-app-74289.firebaseapp.com",
+  projectId: "chat-app-74289",
+  storageBucket: "chat-app-74289.appspot.com",
+  messagingSenderId: "66799785731",
+  appId: "1:66799785731:web:a7c88f71cb4d39fc11fb36"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,3 +18,67 @@ export const auth = getAuth(app);
 export const dbt = getDatabase(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// import { getDatabase } from "firebase/database";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY_2,
+//   authDomain: "nobu-aabd1.firebaseapp.com",
+//   databaseURL: "https://nobu-aabd1-default-rtdb.firebaseio.com",
+//   projectId: "nobu-aabd1",
+//   storageBucket: "nobu-aabd1.appspot.com",
+//   messagingSenderId: "764250910416",
+//   appId: "1:764250910416:web:3c1b57161166ef1995b8d5"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const dbt = getDatabase(app);
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
+// import { getDatabase } from "firebase/database";
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY_3,
+//   authDomain: "chatapp-53682.firebaseapp.com",
+//   projectId: "chatapp-53682",
+//   storageBucket: "chatapp-53682.appspot.com",
+//   messagingSenderId: "725626483555",
+//   appId: "1:725626483555:web:931c44b1069ee8a6201956"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+// export const dbt = getDatabase(app);
+// export const storage = getStorage(app);
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
+// import { getDatabase } from "firebase/database";
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY_4,
+//   authDomain: "dobu-347be.firebaseapp.com",
+//   projectId: "dobu-347be",
+//   storageBucket: "dobu-347be.appspot.com",
+//   messagingSenderId: "1044743627844",
+//   appId: "1:1044743627844:web:1cda7f7c7b6d2a4f330811"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+// export const dbt = getDatabase(app);
+// export const storage = getStorage(app);
